61c22c86b09dbbd9ac65d6bfb0f6644ad9d1e968
Ishant Saini <ishantsaini140588@gmail.com>
2026-01-05 21:53:19 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings
 from typing import Optional
 
 class Settings(BaseSettings):
-    # Database - Railway से आ रहा है
-    DATABASE_URL: str = "mysql://root:GRdCswmwPLXhWbtHGkpAkZxVLnVUsMSM@mysql.railway.internal:3306/railway"
+    # Database - Railway से automatically मिलेगा
+    DATABASE_URL: str
     
     # JWT
-    SECRET_KEY: str = "H9@kL!29sQp#XvA7mZ$8W"  # Railway variable से
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
